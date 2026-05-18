129f016c89b747ba8f460c83114c0ba6cad0a9e0
pjain540 <p.jain@braininventory.com>
2026-05-18 19:11:40 +0530

    remove avatar

--- frontend/src/components/header/AppHeaderDropdown.tsx
+++ frontend/src/components/header/AppHeaderDropdown.tsx
@@ -35,7 +35,7 @@ const AppHeaderDropdown = () => {
           <div className="fw-semibold text-body-emphasis">Hello,</div>
           <div className="text-body-secondary ps-2">{user?.firstName} {user?.lastName}</div>
         </div>
-        <CAvatar src={avatar8} size="md" />
+        {/* <CAvatar src={avatar8} size="md" /> */}
       </CDropdownToggle>
       <CDropdownMenu className="pt-0" placement="bottom-end">
 
